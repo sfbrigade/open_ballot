3b0f72cd1db5196b24ce2c9fa4bd123b0b7452d4
Aaron Borden <adborden@live.com>
2014-03-09 21:31:45 -0700

    ui-router

--- open_ballot/js/app.js
+++ open_ballot/js/app.js
@@ -2,19 +2,24 @@ require('./controllers');
 require('./services');
 
 var app = angular.module('openBallotApp', [
-  'ngRoute',
   'ngResource',
+  'ui.router',
+
   'openBallotControllers',
   'openBallotServices'
 ]);
 
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/', {
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/contracts");
+  $stateProvider
+    .state('contracts', {
+      url: '/contracts',
       templateUrl: 'partials/contracts.tpl.html',
       controller: 'contractsController'
     })
-    .when('/ballots', {
+    .state('ballots', {
+      url: '/ballots',
       templateUrl: 'partials/ballots.tpl.html',
       controller: 'ballotsController'
     });
